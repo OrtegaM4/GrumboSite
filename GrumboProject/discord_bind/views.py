a347ddad55477dac1a7244c95946d9fc65079c8b
Marcos <m.ortega@hotmail.ca>
2018-09-23 03:29:02 -0400

    1

--- GrumboProject/discord_bind/views.py
+++ GrumboProject/discord_bind/views.py
@@ -102,29 +102,26 @@ def tokencall(request):
         return realtoken
     def decompose_data(user, token):
             """ Extract the important details """
-            if 'email' in data:
-                data = {
-                    'uid': user['id'],
-                    'username': user['username'],
-                    'discriminator': user['discriminator'],
-                    'email': user.get('email', ''),
-                    'avatar': user.get('avatar', ''),
-                    'access_token': realtoken,
-                    'scope': ' '.join(token.get('scope', '')),
-                }
-                for k in data:
-                    if data[k] is None:
-                        data[k] = ''
-                try:
-                    expiry = datetime.utcfromtimestamp(float(token['expires_at']))
-                    if settings.USE_TZ:
-                        expiry = make_aware(expiry)
-                    data['expiry'] = expiry
-                except KeyError:
-                    pass
-                return data
-            else:
+            data = {
+                'uid': '',
+                'username': '',
+                'discriminator': '',
+                'email': '',
+                'avatar': '',
+                'access_token': tokencall(request),
+                'scope': '',
+            }
+            for k in data:
+                if data[k] is None:
+                    data[k] = ''
+            try:
+                expiry = datetime.utcfromtimestamp(float(token['expires_at']))
+                if settings.USE_TZ:
+                    expiry = make_aware(expiry)
+                data['expiry'] = expiry
+            except KeyError:
                 pass
+            return data
     def bind_user(request, data):
         """ Create or update a DiscordUser instance """
         uid = data.pop('uid')

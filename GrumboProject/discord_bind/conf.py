f75e85451abfa014689c92ef31c1802b7b002628
Marcos <m.ortega@hotmail.ca>
2018-09-23 22:25:55 -0400

    url swap

--- GrumboProject/discord_bind/conf.py
+++ GrumboProject/discord_bind/conf.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from appconf import AppConf
-MYURL='http://www/grumbot.com/'
+MYURL='http://www.grumbot.com/'
 
 class DiscordBindConf(AppConf):
     """ Application settings """

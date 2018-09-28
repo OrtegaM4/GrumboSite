bf2b6f7acccca758728f7ef3d0c7d1d199c291f0
Marcos <m.ortega@hotmail.ca>
2018-09-28 16:18:10 -0400

    1

--- GrumboProject/discord_bind/views.py
+++ GrumboProject/discord_bind/views.py
@@ -287,10 +287,10 @@ def statsget(request):
           timefix= timeUntilNextBattleInMinutes ##-16
           timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime)/60000))
 
-    # if  timeUntilNextBattleInMinutes < -60  and timeUntilNextBattleInMinutes > -120:
-    #       battlesLeft=battlesLeft+2
-    #       timefix= timeUntilNextBattleInMinutes ##-88
-    #       timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime)/60000))
+    if  timeUntilNextBattleInMinutes < -60  and timeUntilNextBattleInMinutes > -120:
+          battlesLeft=battlesLeft+2
+          timefix= timeUntilNextBattleInMinutes ##-88
+          timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime)/60000))
     #
     # if timeUntilNextBattleInMinutes < -120 and timeUntilNextBattleInMinutes > -180:
     #       battlesLeft=battlesLeft+3

5b4ef233892aa54bcb146836978db5cbcdcd2742
Marcos <m.ortega@hotmail.ca>
2018-09-28 16:16:22 -0400

    test

--- GrumboProject/discord_bind/views.py
+++ GrumboProject/discord_bind/views.py
@@ -290,17 +290,17 @@ def statsget(request):
     if  timeUntilNextBattleInMinutes < -60  and timeUntilNextBattleInMinutes > -120:
           battlesLeft=battlesLeft+2
           timefix= timeUntilNextBattleInMinutes ##-88
-          timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime*2)/60000))
+          timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime)/60000))
 
     if timeUntilNextBattleInMinutes < -120 and timeUntilNextBattleInMinutes > -180:
           battlesLeft=battlesLeft+3
           timefix= timeUntilNextBattleInMinutes
-          timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime*3)/60000))
+          timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime)/60000))
 
     if timeUntilNextBattleInMinutes < -180 and timeUntilNextBattleInMinutes > -240:
           battlesLeft=battlesLeft+4
           timefix= timeUntilNextBattleInMinutes
-          timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime*4)/60000))
+          timeUntilNextBattleInMinutes= math.ceil((timeUntilNextBattleInMinutes - timefix) + (timefix+(waitTime)/60000))
 
     elif timeUntilNextBattleInMinutes  < -240:
           battlesLeft=0

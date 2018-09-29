efb3a090edaad98f76c2434933aab8500e4dd710
Marcos <m.ortega@hotmail.ca>
2018-09-29 19:08:41 -0400

    Filter Test

--- GrumboProject/discord_bind/views.py
+++ GrumboProject/discord_bind/views.py
@@ -178,7 +178,7 @@ shopspecial=db.shop_special
 def get_item(request):
     # url='http://35.182.223.175:5000/api/items'
     r = requests.get('http://35.182.223.175:5000/api/items').json()
-    filteritem = r.filter('warrior')
+    filteritem = r.filter('potion')
 
     print(r)
     return render(request,'grumbo/item.html',context={"filteritem":filteritem})
@@ -186,7 +186,8 @@ def get_item(request):
 def get_equip(request):
     # url='http://35.182.223.175:5000/api/items'
     r = requests.get('http://35.182.223.175:5000/api/equips').json()
-    filterequip = r
+    filterequip = r.filter('warrior')
+
 
 
     print(r)

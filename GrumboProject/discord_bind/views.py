23a11437899b48620538a9b701ba92ef441453c9
Marcos <m.ortega@hotmail.ca>
2018-09-29 20:58:59 -0400

    test

--- GrumboProject/discord_bind/views.py
+++ GrumboProject/discord_bind/views.py
@@ -185,12 +185,12 @@ def get_equip(request):
     r = requests.get('http://35.182.223.175:5000/api/equips').json()
     filterequip = r
 
-    equiplist  = json.loads(r.read())
+    equiplist  = r
 
     d= dict()
 
     for i in equiplist:
-        d[equiplist.classId].append(i)
+        d[equiplist{'classId'}].append(i)
 
 
 # "name": "Rusty Grumblade",

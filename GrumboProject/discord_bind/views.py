2d64aa83955e8fded5fafcc569f5ec853e14ad94
Marcos <m.ortega@hotmail.ca>
2018-09-29 20:54:57 -0400

    test

--- GrumboProject/discord_bind/views.py
+++ GrumboProject/discord_bind/views.py
@@ -182,7 +182,7 @@ def get_item(request):
     return render(request,'grumbo/item.html',context={"filteritem":filteritem})
 
 def get_equip(request):
-    r = requests.get('http://35.182.223.175:5000/api/equips')
+    r = requests.get('http://35.182.223.175:5000/api/equips').json()
     filterequip = r
     equiplist  = json.loads(r)
 
